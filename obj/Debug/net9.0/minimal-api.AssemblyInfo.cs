652aa83bea0d7b603798403d532fe8e0150c1918
unknown <bergamoclara9@gmail.com>
2025-07-21 23:38:15 -0300

    :hammer: validando login e senha no bd

--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8350d63e489ce473b78eeaa67eedbb9556a96e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857aaf8787a49a2534098a999acbbe282846a26a")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
