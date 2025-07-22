ee8b81a0a7499e257d3010bdb7656ac839bf4e46
unknown <bergamoclara9@gmail.com>
2025-07-21 22:36:28 -0300

    :file_folder: estrutura de pastas

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Ola mundo!");
 
-app.MapPost("/login", (LoginDTO loginDTO) =>
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login com sucesso");
@@ -12,11 +12,4 @@ app.MapPost("/login", (LoginDTO loginDTO) =>
 });
 
 app.Run();
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Senha { get; set; } = default!;
-}
-
 
--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79f60562a701610a8417c8d1de14f7e8e3079e1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
